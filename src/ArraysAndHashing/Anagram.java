7299ff8d529612fa178875c715e943aa723429f7
JK Prasad <prasadjk2241@gmail.com>
2026-03-03 07:11:03 +0530

    removed spaces

--- src/ArraysAndHashing/Anagram.java
+++ src/ArraysAndHashing/Anagram.java
@@ -15,18 +15,13 @@ public class Anagram {
         int [] ar1=new int[26];
 
         for (int i=0;i<s.length();i++){
-
-            int i1 = ar1[s.charAt(i) - 'a'];
-            i1 =i1 +1;
-
+             ar1[s.charAt(i) - 'a']++;
         }
 
         int [] ar2=new int[26];
 
         for (int i=0;i<t.length();i++){
-
-            int i1 = ar2[t.charAt(i) - 'a'];
-            i1 =i1 +1;
+            ar2[t.charAt(i) - 'a']++;
         }
 
         return Arrays.equals(ar1,ar2);
